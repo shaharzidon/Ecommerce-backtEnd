a858d55195d76fae9e05f409f6efa420d49295f9
Shahar Zidon <shaharsmacbookpro@Shahars-MacBook-Pro.local>
2022-10-18 09:12:09 +0300

    update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 
@@ -21,11 +22,12 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 app.listen(
     PORT,
